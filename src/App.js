8bf79d96856c9765b0901c971c3cbbf1195253a1
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-07 17:50:49 +0530

    Created NavBar Component as Layout ( using Bootstrap ) and Components for Each Route ( Staff & Home )

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import Staff from './components/pages/Staff'
+
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar/>
+        <div className="container">
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/staff" exact={true} component={Staff}/>
+        </div>  
       </div>
+      </Router>
     );
   }
 }
