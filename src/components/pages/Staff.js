7cc79b8fa791fccee18b886e1e6e55a23080ff3e
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-08 01:55:04 +0530

    Added Individual User's Details Upon Authentication

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,11 +1,30 @@
+import React, { Component } from 'react';
+
+class Staff extends Component {
+
+componentDidMount(){
+  const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+
+  this.setState ({
+    currentUserName: idToken.idToken.claims.email,
+    currentUserEmail: idToken.idToken.claims.name
+  })
+
+}
 
 
-import React from 'react';
+  render() {
 
-const Staff = ({}) => (
-  <div>
-    Staff
-  </div>
-);
+    const  {currentUserName , currentUserEmail}  = this.state;
+
+    return (
+      <div>
+        <h1>Welcome {currentUserName} </h1>
+        <h4>Active Email : {currentUserEmail} </h4>
+      </div>
+    );
+  }
+
+}
 
 export default Staff;
