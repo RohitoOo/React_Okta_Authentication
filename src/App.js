458aa9506c7301a23c4dff06f4662d181622913f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-07 20:43:14 +0530

    Added Secure Route Functionality To Staff Route, Added Okta-React Configuration, Login Component and SignInWidget component from Okta documentation.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,51 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Staff from './components/pages/Staff'
+import {Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
+import Login from './components/auth/Login'
 
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 
 
+const config = {
+  issuer: 'https://poly-swarm2018.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oak38ezp4ysSggcE355'
+}
+
+
+function onAuthRequired({history}){
+  history.push('/login')
+}
+
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <Navbar/>
-        <div className="container">
-          <Route path="/" exact={true} component={Home}/>
-          <Route path="/staff" exact={true} component={Staff}/>
-        </div>  
-      </div>
+        <Security
+          issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+                  onAuthRequired={onAuthRequired}
+          >
+          <div className="App">
+            <Navbar/>
+          <div className="container">
+              <Route path="/" exact={true} component={Home}/>
+               {/* Making Staff Route Secure */}
+              <SecureRoute path="/staff" exact={true} component={Staff}/>
+              <Route
+                path='/login'
+                render={() => (
+                  <Login baseUrl='https://poly-swarm2018.okta.com' />
+              )}
+              />
+              <Route path='/implicit/callback' component={ImplicitCallback}/>
+
+          </div>
+          </div>
+        </Security>
       </Router>
     );
   }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Staff = ({}) => (
-  <div>Staff</div>
+  <div>Home</div>
 );
 
 export default Staff;
